0e6f23ed71326d45662d9131786ef965d6662992
johnsmith0976 <107722299+johnsmith0976@users.noreply.github.com>
2024-07-23 00:28:16 -0400

    changed package.json and updated port variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 const scores = require("./routes/scores")
 const connectDB = require("./db/connect")
 require('dotenv').config()
